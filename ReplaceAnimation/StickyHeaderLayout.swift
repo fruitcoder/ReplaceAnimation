6d2e1f43988a9f2175c42d7bf3e418efa0b9ad5c
drwagh007@gmail.com <drwagh007@gmail.com>
2018-10-27 21:07:35 +0530

    Codebase updated for Swift-4.2

--- ReplaceAnimation/StickyHeaderLayout.swift
+++ ReplaceAnimation/StickyHeaderLayout.swift
@@ -9,176 +9,179 @@
 import UIKit
 
 class StickyHeaderLayout: UICollectionViewFlowLayout {
-  var parallaxHeaderReferenceSize: CGSize? {
-    didSet{
-      self.invalidateLayout()
-    }
-  }
-  
-  var parallaxHeaderMinimumReferenceSize: CGSize = CGSizeZero
-  var parallaxHeaderAlwaysOnTop: Bool = false
-  var disableStickyHeaders: Bool = false
-  
-  override func prepareLayout() {
-    super.prepareLayout()
-  }
-  
-  override func initialLayoutAttributesForAppearingSupplementaryElementOfKind(elementKind: String, atIndexPath elementIndexPath: NSIndexPath) -> UICollectionViewLayoutAttributes? {
-    let attributes = super.initialLayoutAttributesForAppearingSupplementaryElementOfKind(elementKind, atIndexPath: elementIndexPath)
-    var frame = attributes?.frame
-    frame!.origin.y += (self.parallaxHeaderReferenceSize?.height)!
-    attributes?.frame = frame!
-    
-    print(attributes!.frame)
-    
-    return attributes
-  }
-  
-  override func layoutAttributesForSupplementaryViewOfKind(elementKind: String, atIndexPath indexPath: NSIndexPath) -> UICollectionViewLayoutAttributes? {
-    var attributes = super.layoutAttributesForSupplementaryViewOfKind(elementKind, atIndexPath: indexPath)
-    if attributes != nil && elementKind == PullToRefreshHeader.Kind {
-      attributes = StickyHeaderLayoutAttributes(forSupplementaryViewOfKind: elementKind, withIndexPath: indexPath)
-    }
-    
-    return attributes
-  }
-  
-   override func layoutAttributesForElementsInRect(rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
-    var adjustedRec = rect
-    adjustedRec.origin.y -= (self.parallaxHeaderReferenceSize?.height)!
-    
-    var allItems: [UICollectionViewLayoutAttributes] = super.layoutAttributesForElementsInRect(adjustedRec)!
-    
-    let headers: NSMutableDictionary = NSMutableDictionary()
-    let lastCells: NSMutableDictionary = NSMutableDictionary()
-    var visibleParallaxHeader: Bool = false
-    
-    for attributes in allItems {
-      var frame = attributes.frame
-      frame.origin.y += (self.parallaxHeaderReferenceSize?.height)!
-      attributes.frame = frame
-      
-      let indexPath = attributes.indexPath
-      if attributes.representedElementKind == UICollectionElementKindSectionHeader {
-        headers.setObject(attributes, forKey: indexPath.section)
-      } else {
-        let currentAttribute = lastCells.objectForKey(indexPath.section)
-        if currentAttribute == nil || indexPath.row > currentAttribute?.indexPath.row {
-          lastCells.setObject(attributes, forKey: indexPath.section)
+    var parallaxHeaderReferenceSize: CGSize? {
+        didSet{
+            self.invalidateLayout()
         }
-        if indexPath.item == 0 && indexPath.section == 0 {
-          visibleParallaxHeader = true
+    }
+    
+    var parallaxHeaderMinimumReferenceSize: CGSize = .zero
+    var parallaxHeaderAlwaysOnTop: Bool = false
+    var disableStickyHeaders: Bool = false
+    
+    override func prepare() {
+        super.prepare()
+    }
+    
+    override func initialLayoutAttributesForAppearingSupplementaryElement(ofKind elementKind: String, at elementIndexPath: IndexPath) -> UICollectionViewLayoutAttributes? {
+        
+        let attributes = super.initialLayoutAttributesForAppearingSupplementaryElement(ofKind: elementKind, at: elementIndexPath)
+        var frame = attributes?.frame
+        frame!.origin.y += (self.parallaxHeaderReferenceSize?.height)!
+        attributes?.frame = frame!
+        
+        print(attributes!.frame)
+        
+        return attributes
+    }
+    
+    override func layoutAttributesForSupplementaryView(ofKind elementKind: String, at indexPath: IndexPath) -> UICollectionViewLayoutAttributes? {
+        
+        var attributes = super.layoutAttributesForSupplementaryView(ofKind: elementKind, at: indexPath)
+        if attributes != nil && elementKind == PullToRefreshHeader.Kind {
+            attributes = StickyHeaderLayoutAttributes(forSupplementaryViewOfKind: elementKind, with: indexPath)
         }
-      }
-      
-      attributes.zIndex = 1
+        
+        return attributes
     }
     
-    if CGRectGetMinY(rect) <= 0 {
-      visibleParallaxHeader = true
-    }
-    
-    if self.parallaxHeaderAlwaysOnTop == true {
-      visibleParallaxHeader = true
-    }
-    
-    if visibleParallaxHeader && !CGSizeEqualToSize(CGSizeZero, self.parallaxHeaderReferenceSize!) {
-      let currentAttribute = StickyHeaderLayoutAttributes(forSupplementaryViewOfKind: PullToRefreshHeader.Kind, withIndexPath: NSIndexPath(index: 0))
-      var frame = currentAttribute.frame
-      frame.size.width = (self.parallaxHeaderReferenceSize?.width)!
-      frame.size.height = (self.parallaxHeaderReferenceSize?.height)!
-      
-      let bounds = self.collectionView?.bounds
-      let maxY = CGRectGetMaxY(frame)
-      
-      var y = min(maxY - self.parallaxHeaderMinimumReferenceSize.height, (bounds?.origin.y)! + (self.collectionView?.contentInset.top)!)
-      let height = max(0, -y + maxY)
-      
-      
-      let maxHeight = self.parallaxHeaderReferenceSize!.height
-      let minHeight = self.parallaxHeaderMinimumReferenceSize.height
-      let progressiveness = (height - minHeight)/(maxHeight-minHeight)
-      currentAttribute.progress = progressiveness
-      
-      currentAttribute.zIndex = 0
-      
-      if self.parallaxHeaderAlwaysOnTop {// && height <= self.parallaxHeaderMinimumReferenceSize.height {
-        let insertTop = self.collectionView?.contentInset.top
-        y = (self.collectionView?.contentOffset.y)! + insertTop!
-        currentAttribute.zIndex = 2000
-      }
-      
-      currentAttribute.frame = CGRectMake(frame.origin.x, y, frame.size.width, height)
-      allItems.append(currentAttribute)
-    }
-    
-    if !self.disableStickyHeaders {
-      for obj in lastCells.keyEnumerator() {
-        if let indexPath = obj.indexPath {
-          let indexPAthKey = indexPath.section
-          
-          var header = headers[indexPAthKey]
-          if header == nil {
-            header = self.layoutAttributesForSupplementaryViewOfKind(UICollectionElementKindSectionHeader, atIndexPath: NSIndexPath(forItem: 0, inSection: indexPath.section))
-            if let header:UICollectionViewLayoutAttributes = header as? UICollectionViewLayoutAttributes {
-              allItems.append(header)
+    override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
+        var adjustedRec = rect
+        adjustedRec.origin.y -= (self.parallaxHeaderReferenceSize?.height)!
+        
+        var allItems: [UICollectionViewLayoutAttributes] = super.layoutAttributesForElements(in: adjustedRec)!
+        
+        let headers: NSMutableDictionary = NSMutableDictionary()
+        let lastCells: NSMutableDictionary = NSMutableDictionary()
+        var visibleParallaxHeader: Bool = false
+        
+        for attributes in allItems {
+            var frame = attributes.frame
+            frame.origin.y += (self.parallaxHeaderReferenceSize?.height)!
+            attributes.frame = frame
+            
+            let indexPath = attributes.indexPath
+            if attributes.representedElementKind == UICollectionView.elementKindSectionHeader {
+                headers.setObject(attributes, forKey: indexPath.section as NSCopying)
+            } else {
+                let currentAttribute = lastCells.object(forKey: indexPath.section)
+                if currentAttribute == nil || indexPath.row > (currentAttribute as AnyObject).indexPath.row {
+                    lastCells.setObject(attributes, forKey: indexPath.section as NSCopying)
+                }
+                if indexPath.item == 0 && indexPath.section == 0 {
+                    visibleParallaxHeader = true
+                }
             }
-          }
-          
-          self.updateHeaderAttributesForLastCellAttributes(header as! UICollectionViewLayoutAttributes, lastCellAttributes: lastCells[indexPAthKey] as! UICollectionViewLayoutAttributes)
+            
+            attributes.zIndex = 1
         }
-      }
+        
+        if rect.minY <= 0 {
+            visibleParallaxHeader = true
+        }
+        
+        if self.parallaxHeaderAlwaysOnTop == true {
+            visibleParallaxHeader = true
+        }
+        
+        if visibleParallaxHeader && !CGSize.zero.equalTo(self.parallaxHeaderReferenceSize!) {
+            let currentAttribute = StickyHeaderLayoutAttributes(forSupplementaryViewOfKind: PullToRefreshHeader.Kind, with: NSIndexPath(index: 0) as IndexPath)
+            var frame = currentAttribute.frame
+            frame.size.width = (self.parallaxHeaderReferenceSize?.width)!
+            frame.size.height = (self.parallaxHeaderReferenceSize?.height)!
+            
+            let bounds = self.collectionView?.bounds
+            let maxY = frame.maxY
+            
+            var y = min(maxY - self.parallaxHeaderMinimumReferenceSize.height, (bounds?.origin.y)! + (self.collectionView?.contentInset.top)!)
+            let height = max(0, -y + maxY)
+            
+            
+            let maxHeight = self.parallaxHeaderReferenceSize!.height
+            let minHeight = self.parallaxHeaderMinimumReferenceSize.height
+            let progressiveness = (height - minHeight)/(maxHeight-minHeight)
+            currentAttribute.progress = progressiveness
+            
+            currentAttribute.zIndex = 0
+            
+            if self.parallaxHeaderAlwaysOnTop {// && height <= self.parallaxHeaderMinimumReferenceSize.height {
+                let insertTop = self.collectionView?.contentInset.top
+                y = (self.collectionView?.contentOffset.y)! + insertTop!
+                currentAttribute.zIndex = 2000
+            }
+            
+            currentAttribute.frame = CGRect(x: frame.origin.x, y: y, width: frame.size.width, height: height)
+            allItems.append(currentAttribute)
+        }
+        
+        if !self.disableStickyHeaders {
+            for obj in lastCells.keyEnumerator() {
+                if let indexPath = (obj as AnyObject).indexPath {
+                    let indexPAthKey = indexPath.section
+                    
+                    var header = headers[indexPAthKey]
+                    if header == nil {
+                        header = self.layoutAttributesForSupplementaryView(ofKind: UICollectionView.elementKindSectionHeader, at: IndexPath(item: 0, section: indexPath.section))
+                        if let header:UICollectionViewLayoutAttributes = header as? UICollectionViewLayoutAttributes {
+                            allItems.append(header)
+                        }
+                    }
+                    
+                    
+                    self.updateHeaderAttributesForLastCellAttributes(attributes: header as! UICollectionViewLayoutAttributes, lastCellAttributes: lastCells[indexPAthKey] as! UICollectionViewLayoutAttributes)
+                }
+            }
+        }
+        
+        return allItems
     }
     
-    return allItems
-  }
-  
-  override func layoutAttributesForItemAtIndexPath(indexPath: NSIndexPath) -> UICollectionViewLayoutAttributes? {
-    if let attributes = super.layoutAttributesForItemAtIndexPath(indexPath)?.copy() as? UICollectionViewLayoutAttributes {
-      var frame = attributes.frame
-      frame.origin.y += (self.parallaxHeaderReferenceSize?.height)!
-      attributes.frame = frame
-      return attributes
-    } else {
-      return nil
-    }
-  }
-  
-  override func collectionViewContentSize() -> CGSize {
-    if self.collectionView?.superview == nil {
-      return CGSizeZero
+    override func layoutAttributesForItem(at indexPath: IndexPath) -> UICollectionViewLayoutAttributes? {
+        if let attributes = super.layoutAttributesForItem(at: indexPath)?.copy() as? UICollectionViewLayoutAttributes {
+            var frame = attributes.frame
+            frame.origin.y += (self.parallaxHeaderReferenceSize?.height)!
+            attributes.frame = frame
+            return attributes
+        } else {
+            return nil
+        }
     }
     
-    var size = super.collectionViewContentSize()
-    size.height += (self.parallaxHeaderReferenceSize?.height)!
-    return size
-  }
-  
-  override func shouldInvalidateLayoutForBoundsChange(newBounds: CGRect) -> Bool {
-    return true
-  }
-  
-  //MARK: Helper
-  
-  func updateHeaderAttributesForLastCellAttributes(attributes:UICollectionViewLayoutAttributes, lastCellAttributes: UICollectionViewLayoutAttributes) {
-    let currentBounds = self.collectionView?.bounds
-    attributes.zIndex = 1024
-    attributes.hidden = false
-    
-    var origin = attributes.frame.origin
-    
-    let sectionMaxY = CGRectGetMaxY(lastCellAttributes.frame) - attributes.frame.size.height
-    var y = CGRectGetMaxY(currentBounds!) - (currentBounds?.size.height)! + (self.collectionView?.contentInset.top)!
-    
-    if self.parallaxHeaderAlwaysOnTop {
-      y += self.parallaxHeaderMinimumReferenceSize.height
+    override var collectionViewContentSize: CGSize {
+        if self.collectionView?.superview == nil {
+            return .zero
+        }
+        
+        var size = super.collectionViewContentSize
+        size.height += (self.parallaxHeaderReferenceSize?.height)!
+        return size
     }
     
-    let maxY = min(max(y, attributes.frame.origin.y), sectionMaxY)
+    override func shouldInvalidateLayout(forBoundsChange newBounds: CGRect) -> Bool {
+        return true
+    }
     
-    origin.y = maxY
+    //MARK: Helper
     
-    attributes.frame = CGRectMake(origin.x, origin.y, attributes.frame.size.width, attributes.frame.size.width)
-    print(attributes.frame)
-  }
+    func updateHeaderAttributesForLastCellAttributes(attributes:UICollectionViewLayoutAttributes, lastCellAttributes: UICollectionViewLayoutAttributes) {
+        let currentBounds = self.collectionView?.bounds
+        attributes.zIndex = 1024
+        attributes.isHidden = false
+        
+        var origin = attributes.frame.origin
+        
+        let sectionMaxY = lastCellAttributes.frame.maxY - attributes.frame.size.height
+        var y = currentBounds!.maxY - (currentBounds?.size.height)! + (self.collectionView?.contentInset.top)!
+        
+        if self.parallaxHeaderAlwaysOnTop {
+            y += self.parallaxHeaderMinimumReferenceSize.height
+        }
+        
+        let maxY = min(max(y, attributes.frame.origin.y), sectionMaxY)
+        
+        origin.y = maxY
+        
+        attributes.frame = CGRect(x: origin.x, y:origin.y, width: attributes.frame.size.width, height: attributes.frame.size.width)
+        print(attributes.frame)
+    }
 }
--- ReplaceAnimation/ThreeDotButton.swift
+++ ReplaceAnimation/ThreeDotButton.swift
@@ -9,31 +9,31 @@
 import UIKit
 
 @IBDesignable class ThreeDotButton: UIButton {
-  @IBInspectable var verticalInset : CGFloat = 10.0 {
-    didSet { setNeedsDisplay() }
-  }
-  
-  @IBInspectable var dotRadius : CGFloat = 2.0 {
-    didSet { setNeedsDisplay() }
-  }
-  
-  override func drawRect(rect: CGRect) {
-    let insetRect = CGRectInset(rect, CGRectGetMidX(rect) - floor(dotRadius / 2.0), verticalInset)
-    let padding = CGFloat((CGRectGetHeight(insetRect) - 6.0 * dotRadius) / 2.0)// vertical padding between dots
-    let center1 = CGPointMake(CGRectGetMidX(insetRect), insetRect.origin.y + dotRadius)
-    let center2 = CGPointMake(CGRectGetMidX(insetRect), insetRect.origin.y + 3.0 * dotRadius + padding)
-    let center3 = CGPointMake(CGRectGetMidX(insetRect), insetRect.origin.y + 5.0 * dotRadius + 2.0 * padding)
+    @IBInspectable var verticalInset : CGFloat = 10.0 {
+        didSet { setNeedsDisplay() }
+    }
     
-    // Bezier Drawing
-    let bezierPath = UIBezierPath()
-    bezierPath.moveToPoint(center1)
-    bezierPath.addArcWithCenter(center1, radius: dotRadius, startAngle: 0, endAngle: CGFloat(2.0*M_PI), clockwise: true)
-    bezierPath.moveToPoint(center2)
-    bezierPath.addArcWithCenter(center2, radius: dotRadius, startAngle: 0, endAngle: CGFloat(2.0*M_PI), clockwise: true)
-    bezierPath.moveToPoint(center3)
-    bezierPath.addArcWithCenter(center3, radius: dotRadius, startAngle: 0, endAngle: CGFloat(2.0*M_PI), clockwise: true)
+    @IBInspectable var dotRadius : CGFloat = 2.0 {
+        didSet { setNeedsDisplay() }
+    }
     
-    tintColor.setFill()
-    bezierPath.fill()
-  }
+    override func draw(_ rect: CGRect) {
+        let insetRect = rect.insetBy(dx: rect.midX - floor(dotRadius / 2.0), dy: verticalInset)
+        let padding = CGFloat((insetRect.height - 6.0 * dotRadius) / 2.0)// vertical padding between dots
+        let center1 = CGPoint(x: insetRect.midX,y: insetRect.origin.y + dotRadius)
+        let center2 = CGPoint(x: insetRect.midX, y: insetRect.origin.y + 3.0 * dotRadius + padding)
+        let center3 = CGPoint(x: insetRect.midX, y: insetRect.origin.y + 5.0 * dotRadius + 2.0 * padding)
+        
+        // Bezier Drawing
+        let bezierPath = UIBezierPath()
+        bezierPath.move(to: center1)
+        bezierPath.addArc(withCenter: center1, radius: dotRadius, startAngle: 0, endAngle: CGFloat(2.0*Double.pi), clockwise: true)
+        bezierPath.move(to: center2)
+        bezierPath.addArc(withCenter: center2, radius: dotRadius, startAngle: 0, endAngle: CGFloat(2.0*Double.pi), clockwise: true)
+        bezierPath.move(to: center3)
+        bezierPath.addArc(withCenter: center3, radius: dotRadius, startAngle: 0, endAngle: CGFloat(2.0*Double.pi), clockwise: true)
+        
+        tintColor.setFill()
+        bezierPath.fill()
+    }
 }
